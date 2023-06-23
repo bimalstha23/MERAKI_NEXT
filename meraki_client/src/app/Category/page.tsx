4d5dc7ba8440ded79efa7bc6f55bf149019e4098
rikesh <70895896+rikeshhh@users.noreply.github.com>
2023-06-24 00:25:09 +0545

    built in error fixed

--- meraki_client/src/app/Category/page.tsx
+++ meraki_client/src/app/Category/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 
-export const Category = () => {
+const Category = () => {
   return (
+    <div>
+        return (
     <div className="flex flex-col justify-center items-center border bg-primary">
     <h1>Necklace</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sapiente mollitia harum ratione distinctio nobis architecto? Iste sunt voluptatem, repellendus accusamus soluta quaerat, ad dolores, aspernatur odit cupiditate quisquam quis!</p>
     <button>Shop Now</button>      
 </div>
+    </div>
   )
 }
+
+export default Category

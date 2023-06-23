d64557b56cbf2966547961a76e2fb4222f421830
rikesh <70895896+rikeshhh@users.noreply.github.com>
2023-06-23 19:10:02 +0545

    responsive on mobile version/made navbars right

--- meraki_client/src/app/NavBar/page.tsx
+++ meraki_client/src/app/NavBar/page.tsx
@@ -1,7 +1,64 @@
+'use client'
+import { Gudea } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import {GiHamburgerMenu} from "react-icons/gi"
+import { Drawer } from "@mui/material";
+import { ImCross } from "react-icons/im";
+import React, { useState } from 'react';
+import { Theme, useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+const names = [
+  'EARRINGS',
+  'HAIR STICKS',
+  'RINGS',
+  'BRACELETES',
+  'NECKLACE',
+  'CLIPS',
+];
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 export const NavBar = () => {
+  const [searchparam, setSearchParam] = useState<string>("");
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setShowMenu(false);
+  };
+  const showmenu = showMenu ? "block " : "hidden";
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+
   return (
   <div className='flex flex-col'>
     <div className='flex flex-row justify-center items-center bg-gray gap-9'>
@@ -11,8 +68,11 @@ export const NavBar = () => {
       width={61}
       alt="Picture of the author"
     />
+      <button className=" sm:hidden max-sm:block text-white">
+            <GiHamburgerMenu size={30} onClick={() => setShowMenu(true)} />
+          </button>
      <div className="relative text-gray-600">
-        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-20 rounded-lg text-sm focus:outline-none"
+        <input className="border-2 border-gray-300 bg-white h-10 px-5 max-sm:px-0 max-sm:pr-0 pr-20 rounded-lg text-sm focus:outline-none"
           type="search" name="search" placeholder="Search"/>
         <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
           <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
@@ -23,18 +83,54 @@ export const NavBar = () => {
           </svg>
         </button>
       </div>
-      <div className='flex text-white gap-4 font-black'>
+      <div className='flex text-white gap-4 font-black max-sm:hidden'>
 <Link href="">Home</Link>
 <Link href="">Collection</Link>
 <Link href="">Products</Link>
       </div>
-      <div className='flex text-white gap-5'>
+      <div className='flex text-white gap-5 max-sm:hidden'>
     <Link href="">Cart</Link>
     <Link href="">Account</Link>
       </div>
     </div>
 
     <div className='flex flex-row justify-center items-center bg-primary text-black gap-4 p-2'>
+
+<div className='md:hidden'>
+<FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+        <Select
+          multiple
+          displayEmpty
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <em>All Categories</em>;
+            }
+
+            return selected.join(', ');
+          }}
+          MenuProps={MenuProps}
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem disabled value="">
+            <em>All Categories</em>
+          </MenuItem>
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+</div>
+<div className='max-sm:hidden'>
+
 <Link href="">ALL CATEGORIES</Link>
 <Link href="">EARRINGS</Link>
 <Link href="">HAIR STICKS</Link>
@@ -42,7 +138,36 @@ export const NavBar = () => {
 <Link href="">BRACELETS</Link>
 <Link href="">NECKLACE</Link>
 <Link href="">CLIPS</Link>
+</div>
     </div>
+  
+    <div>
+    <Drawer
+        open={showMenu}
+        onClose={handleClose}
+        anchor="right"
+        className="max-sm:block hidden"
+      >
+        <div className="h-full bg-black flex justify-center items-center flex-col">
+          <button className="text-white pt-16">
+            <ImCross size={40} onClick={() => setShowMenu(false)} />
+          </button>
+          <div className="max-sm:flex flex-col justify-center items-center   w-[50vh] bg-black h-full ">
+            <div className={`${showmenu} max-sm:block w-full text-center text-white`}>
+              <ul className="list-none flex flex-col gap-10">
+              <Link href="">Home</Link>
+<Link href="">Collection</Link>
+<Link href="">Products</Link>
+<Link href="">Cart</Link>
+<Link href="">Account</Link>
+              </ul>
+            
+            </div>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+   
   </div>
   )
 }

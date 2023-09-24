9e0e6ad9702c6a9f9343c5b689bfb2c5ad4eacc8
Bimal Shrestha <bimalshrestha074@gmail.com>
2023-09-24 21:14:25 +0545

    headless ui fixed

--- client/src/app/products/components/ProductsPage.tsx
+++ client/src/app/products/components/ProductsPage.tsx
@@ -152,7 +152,7 @@ const ProductsPage = () => {
                             </div>
 
                             <Transition
-                                as="Fragment"
+                                as={Fragment}
                                 enter="transition ease-out duration-100"
                                 enterFrom="transform opacity-0 scale-95"
                                 enterTo="transform opacity-100 scale-100"
